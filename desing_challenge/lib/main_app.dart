9a93cf7deb4eeaedd891aae20376f62e22d6f19a
Kenny1942 <w.kenny.mohr@gmail.com>
2025-05-06 12:21:19 +0200

    2commit

--- desing_challenge/lib/main_app.dart
+++ desing_challenge/lib/main_app.dart
@@ -6,6 +6,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Screen1());
+    return const MaterialApp(
+      home: Screen1(),
+    );
   }
 }
