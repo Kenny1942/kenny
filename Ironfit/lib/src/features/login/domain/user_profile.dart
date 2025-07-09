d8910722613f4f920df759a3f9b06ce13deaa3d5
Kenny1942 <w.kenny.mohr@gmail.com>
2025-07-09 09:40:20 +0200

    des and serialization

--- Ironfit/lib/src/features/login/domain/user_profile.dart
+++ Ironfit/lib/src/features/login/domain/user_profile.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 enum Gender { male, female, other }
 
 class UserProfile {
@@ -18,4 +20,28 @@ class UserProfile {
     required this.height,
     required this.gender,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'userId': userId,
+      'name': name,
+      'dateOfBirth': dateOfBirth,
+      'weight': weight,
+      'height': height,
+      'gender': gender.name,
+    };
+  }
+
+  factory UserProfile.fromMap(Map<String, dynamic> map) {
+    return UserProfile(
+      id: map['id'],
+      userId: map['userId'],
+      name: map['name'],
+      dateOfBirth: (map['dateOfBirth'] as Timestamp).toDate(),
+      weight: map['weight'],
+      height: map['heigth'],
+      gender: Gender.values.byName(map['gender.name']),
+    );
+  }
 }
